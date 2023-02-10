b8cbd60ca38e8040a4f50daf785b929ca4acdab5
Sanjaya <sanjaya.maharana@iail.com>
2023-02-11 05:09:08 +0530

    Start

--- app.py
+++ app.py
@@ -17,22 +17,22 @@ file.close()
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def index():
+def home():
     if request.method == "POST":
 
         url = request.form["url"]
         obj = FeatureExtraction(url)
-        x = np.array(obj.getFeaturesList()).reshape(1,30) 
+        x = np.array(obj.getFeaturesList()).reshape(1,30)
 
         y_pred =gbc.predict(x)[0]
-        #1 is safe       
+        #1 is safe
         #-1 is unsafe
         y_pro_phishing = gbc.predict_proba(x)[0,0]
         y_pro_non_phishing = gbc.predict_proba(x)[0,1]
         # if(y_pred ==1 ):
         pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
-        return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
-    return render_template("index.html", xx =-1)
+        return render_template('home.html',xx =round(y_pro_non_phishing,2),url=url )
+    return render_template("home.html", xx =-1)
 
 
 if __name__ == "__main__":
